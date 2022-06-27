126485d658e7f987e9d9a6e756b245c8cca24137
ssrrajan <ssrrajan@gmail.com>
2022-06-27 16:27:48 +0530

    update

--- learning_templates/learning_templates/settings.py
+++ learning_templates/learning_templates/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-e8c!f!)^m5hxo26se)q-p7=cq1d3_+*-qytq&4fxpb!rpilgv0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ssrrajan.pythonanywhere.com']
 
 
 # Application definition
